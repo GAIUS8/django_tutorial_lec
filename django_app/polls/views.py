5317b3c22c6e0746360624480bcb4403b8cffaa9
GAIUS8 <gaius827@gmail.com>
2017-06-02 01:29:05 +0900

    make detail, result, vote in Views / set the admin and polls.urls

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello world')
+
+
+def detail(request, question_id):
+    return HttpResponse("you're looking at the question {}".format(question_id))
+
+
+def results(request, question_id):
+    resoponse = "you're looking at the results of question {}"
+    return HttpResponse(resoponse.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question {}".format(question_id))
