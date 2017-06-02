e08bceed36b526f6c4812d98cc3080b30199c7fa
GAIUS8 <gaius827@gmail.com>
2017-06-02 16:45:41 +0900

    modify admin page

--- django_app/polls/admin.py
+++ django_app/polls/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ('pub_date', 'question_text',)
+    list_display = (
+        'question_text',
+        'pub_date',
+        'was_published_recently',
+    )
+    fieldsets = [
+        (None, {
+            'fields': [
+                'question_text'
+            ]
+        }),
+        ('Date Information', {
+            'fields': [
+                'pub_date',
+            ]
+        })
+    ]
+    list_filter = ['pub_date']
+    inlines = (ChoiceInline,)
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
